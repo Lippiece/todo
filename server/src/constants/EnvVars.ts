33be8fcd40d5142c28321fd21c07239017f297e1
lippiece <github@lippiece.anonaddy.me>
2024-01-21 16:46:03 +0300

    chore(files): clean project files

--- server/src/constants/EnvVars.ts
+++ server/src/constants/EnvVars.ts
@@ -5,24 +5,29 @@
 /* eslint-disable node/no-process-env */
 
 export default {
-  NodeEnv: process.env.NODE_ENV ?? "",
-  Port: process.env.PORT ?? 3000,
   CookieProps: {
     Key: "ExpressGeneratorTs",
-    Secret: process.env.COOKIE_SECRET ?? "",
+
+
     // Casing to match express cookie options
     Options: {
-      httpOnly: true,
-      signed: true,
-      path: process.env.COOKIE_PATH ?? "",
-      maxAge: Number(process.env.COOKIE_EXP ?? 0),
       domain: process.env.COOKIE_DOMAIN ?? "",
+      httpOnly: true,
+      maxAge: Number(process.env.COOKIE_EXP ?? 0),
+      path: process.env.COOKIE_PATH ?? "",
       secure: process.env.SECURE_COOKIE === "true",
+      signed: true,
     },
+
+    Secret: process.env.COOKIE_SECRET ?? "",
   },
+
   Jwt: {
-    Secret: process.env.JWT_SECRET ?? "",
     Exp: process.env.COOKIE_EXP ?? "", // exp at the same time as the cookie
+    Secret: process.env.JWT_SECRET ?? "",
   },
+
   MONGO: process.env.MONGO ?? "",
+  NodeEnv: process.env.NODE_ENV ?? "",
+  Port: process.env.PORT ?? 3000,
 } as const
