7ef2589b1e542929f5c7c7034576dcbd9a22622e
lippiece <github@lippiece.anonaddy.me>
2024-01-21 16:45:40 +0300

    feat(db): add TaskList model

---
+++ server/src/models/TaskList.ts
@@ -0,0 +1,23 @@
+import { model, type ObjectId, Schema, Types } from "mongoose"
+
+export interface ITaskList {
+  _id: string
+  createdAt: Date
+  description: string
+  name: string
+  tasks: ObjectId[]
+  updatedAt: Date
+}
+
+const taskListSchema = new Schema<ITaskList>({
+  _id: { required: true, type: String },
+  createdAt: { required: true, type: Date },
+  description: { required: true, type: String },
+  name: { required: true, type: String },
+  tasks: { ref: "Task", required: true, type: [Types.ObjectId] },
+  updatedAt: { required: true, type: Date },
+})
+
+const TaskList = model<ITaskList>("TaskList", taskListSchema)
+
+export default TaskList
