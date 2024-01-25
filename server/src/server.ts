ac2289541bc6a8c924cd0d7acc41d52658cd3e75
lippiece <github@lippiece.anonaddy.me>
2024-01-25 18:48:29 +0200

    fix(server): use permissive cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -44,12 +44,13 @@ if (EnvVars.NodeEnv === NodeEnvs.Dev.valueOf()) {
   app.use(
     morgan(":method :url :status :res[content-length] - :response-time ms"),
   )
-  app.use(cors())  
 }
 
-app.use(cors({
-    origin: ["https://lippiece.github.io/todo/", "https://lippiece.github.io/"]
-}))
+app.use(cors())  
+
+//app.use(cors({
+  //  origin: ["https://lippiece.github.io/todo/", "https://lippiece.github.io/"]
+//}))
 
 // Security
 if (EnvVars.NodeEnv === NodeEnvs.Production.valueOf()) {
